15ee6786acfb8ce84c34c57361b4b286f5852429
24106011-svg <24106011@nec.edu.in>
2026-03-06 15:04:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function addTask(){
+
+let input = document.getElementById("taskInput");
+let task = input.value;
+
+if(task === ""){
+alert("Enter a task");
+return;
+}
+
+let li = document.createElement("li");
+
+li.innerHTML = `
+<span onclick="completeTask(this)">${task}</span>
+<button onclick="deleteTask(this)">Delete</button>
+`;
+
+document.getElementById("taskList").appendChild(li);
+
+input.value="";
+}
+
+function deleteTask(btn){
+btn.parentElement.remove();
+}
+
+function completeTask(task){
+task.classList.toggle("completed");
+}
